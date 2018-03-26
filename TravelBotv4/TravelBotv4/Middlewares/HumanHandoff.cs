445cdea7f5e7f4f699c09adff5fe0fa80f2afd0a
OHNISHI\hiroaki-honda <hiroaki-honda@navitime.co.jp>
2018-03-26 21:38:49 +0900

    Modify human connecting middleware logic

--- TravelBotv4/TravelBotv4/Middlewares/HumanHandoff.cs
+++ TravelBotv4/TravelBotv4/Middlewares/HumanHandoff.cs
@@ -1,39 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Microsoft.Bot.Schema;
 using Microsoft.Bot.Builder;
-using Microsoft.Bot.Connector;
 using Underscore.Bot.MessageRouting;
-using Underscore.Bot.MessageRouting.DataStore.Azure;
-using Underscore.Bot.MessageRouting.DataStore.Local;
+using TravelBotv4.MessageRouting;
+using TravelBotv4.CommandHandling;
 
 
 namespace TravelBotv4.Middlewares
 {
     public class HumanHandoff : IMiddleware
     {
-        public Task OnProcessRequest(IBotContext context, MiddlewareSet.NextDelegate next)
+        public async Task OnProcessRequest(IBotContext context, MiddlewareSet.NextDelegate next)
         {
-            MessageRouterManager messageRouterManager = Startup.MessageRouterManager;
             var activity = context.Request.AsMessageActivity();
 
-            string message = context.Request.Text;
-            if(hasKeyword(message))
+            if (activity.Type == ActivityTypes.Message)
             {
-                // TODO Implement the logic to connect to agent
-                return null;
-            }
-            else
-            {
-                return next();
-            }
-            throw new NotImplementedException();
-        }
+                MessageRouterManager messageRouterManager = Startup.MessageRouterManager;
+                MessageRouterResultHandler messageRouterResultHandler = Startup.MessageRouterResultHandler;
+                bool rejectConnectionRequestIfNoAggregationChannel =
+                    Startup.Settings.RejectConnectionRequestIfNoAggregationChannel;
 
-        private bool hasKeyword(string message)
-        {
-            return message.Contains("human");
+                messageRouterManager.MakeSurePartiesAreTracked(activity);
+
+                // First check for commands (both from back channel and the ones directly typed)
+                MessageRouterResult messageRouterResult =
+                    Startup.BackChannelMessageHandler.HandleBackChannelMessage((Activity)activity);
+
+                if (messageRouterResult.Type != MessageRouterResultType.Connected
+                    && await Startup.CommandMessageHandler.HandleCommandAsync((Activity)activity) == false)
+                {
+                    // No valid back channel (command) message or typed command detected
+
+                    // Let the message router manager instance handle the activity
+                    messageRouterResult = await messageRouterManager.HandleActivityAsync(
+                        activity, false, rejectConnectionRequestIfNoAggregationChannel);
+
+                    if (messageRouterResult.Type == MessageRouterResultType.NoActionTaken)
+                    {
+                        // No action was taken by the message router manager. This means that the
+                        // user is not connected (in a 1:1 conversation) with a human
+                        // (e.g. customer service agent) yet.
+                        //
+                        // You can, for example, check if the user (customer) needs human
+                        // assistance here or forward the activity to a dialog. You could also do
+                        // the check in the dialog too...
+                        //
+                        // Here's an example:
+                        if (!string.IsNullOrEmpty(activity.Text)
+                            && activity.Text.ToLower().Contains(Commands.CommandRequestConnection))
+                        {
+                            messageRouterResult = messageRouterManager.RequestConnection(
+                                activity, rejectConnectionRequestIfNoAggregationChannel);
+                        }
+                        else
+                        {
+                            await next();
+                        }
+                    }
+                }
+
+                // Uncomment to see the result in a reply (may be useful for debugging)
+                //await MessagingUtils.ReplyToActivityAsync(activity, messageRouterResult.ToString());
+
+                // Handle the result, if required
+                await messageRouterResultHandler.HandleResultAsync(messageRouterResult);
+            }
         }
     }
 }
