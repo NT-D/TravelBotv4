d9885829f1817c556981c32865a218b402542e8f
Masayuki Ota <masota@microsoft.com>
2018-03-26 14:59:01 +0900

    Add log middleware mock

---
+++ TravelBotv4/TravelBotv4/Middlewares/LogMiddleware.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+
+namespace TravelBotv4.Middlewares
+{
+    public class LogMiddleware : IMiddleware
+    {
+        public Task OnProcessRequest(IBotContext context, MiddlewareSet.NextDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
