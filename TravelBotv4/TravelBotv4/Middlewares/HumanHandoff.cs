a09fec9b02674be1f9f9a4e35312e8e257aac895
OHNISHI\hiroaki-honda <hiroaki-honda@navitime.co.jp>
2018-03-28 18:01:54 +0900

    Modify human handsoff middleware

--- TravelBotv4/TravelBotv4/Middlewares/HumanHandoff.cs
+++ TravelBotv4/TravelBotv4/Middlewares/HumanHandoff.cs
@@ -1,14 +1,19 @@
 ﻿using System.Threading.Tasks;
+using System;
 using Microsoft.Bot.Schema;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.BotFramework;
 using Microsoft.Bot.Builder.Core;
 using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.Bot.Builder.Integration.AspNet.WebApi;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 using Underscore.Bot.MessageRouting;
 using TravelBotv4.MessageRouting;
 using TravelBotv4.CommandHandling;
 using TravelBotv4.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace TravelBotv4.Middlewares
 {
@@ -18,11 +23,32 @@ namespace TravelBotv4.Middlewares
         {
             var activity = context.Request.AsMessageActivity();
 
-            if (activity.Type == ActivityTypes.Event && activity.From.Id == "ConversationOwner")
+            // Handle the message from functions contains ChatPlus's webhook response
+            if (activity.Type == ActivityTypes.Event)
             {
-                // TODO hiroaki-honda Implement logic to send proactive message to user
+                string userId = Deserialize<Visitor>("visitor", (Activity)activity).visitor_id;
+                ConversationReference conversationReference = GetConversationReferenceByUserId(userId);
+
+                switch (activity.From.Id)
+                {
+                    case "WebhookStartChat":
+                        await SendProactiveMessage(context, conversationReference);
+                        break;
+                    case "WebhookSendMessage":
+                        await SendProactiveMessage(context, conversationReference);
+                        break;
+                    default:
+                        throw new Exception("unexpected event type message");
+                }
             }
 
+            var connectionState = context.GetConversationState<ConnectionState>();
+            if (connectionState.IsConnectedToAgent)
+            {
+                // TODO hiroaki-honda Hook the function which send message to agent
+            }
+
+            // Request to make a connection between user and agent
             if (!string.IsNullOrEmpty(activity.Text)
                 && activity.Text.ToLower().Contains(Commands.CommandRequestConnection))
             {
@@ -43,7 +69,7 @@ namespace TravelBotv4.Middlewares
                 TableOperation insertOperation = TableOperation.Insert(conversationInformation);
 
                 // TODO hiroaki-honda Implement the logic to hook the function which request connection to ChatPlus
-
+                // Status: Ask chatplus to prepare the API which receive the request to connect to agent
 
                 // Set connecting state true 
                 var state = context.GetConversationState<ConnectionState>();
@@ -70,6 +96,23 @@ namespace TravelBotv4.Middlewares
             };
 
             return r;
+        }
+
+        private async Task SendProactiveMessage(IBotContext context, ConversationReference conversationReference)
+        {
+            // TODO hiroaki-honda Implement logic to send proactive message to user
+            //context.Adapter.ContinueConversation()
+        }
+
+        private T Deserialize<T>(string name, Activity activity)
+        {
+            return JsonConvert.DeserializeObject<T>(((JObject)activity.Value).GetValue(name).ToString());
+        }
+
+        private ConversationReference GetConversationReferenceByUserId(string userId)
+        {
+            // TODO hiroaki-honda Implement the logic to get ConversationReferenc from table storage using userId as key
+            return null;
         }
     }
 }
---
+++ TravelBotv4/TravelBotv4/Models/ChatPlusWebhook.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelBotv4.Models
+{
+    //public class ChatPlusWebhook
+    //{
+
+    //}
+    public class ChatPlusInformation
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public string type { get; set; }
+        public string timestamp { get; set; }
+        public Site site { get; set; }
+        public Room room { get; set; }
+        public Agent agent { get; set; }
+        public Visitor visitor { get; set; }
+    }
+
+    public class Site
+    {
+        public int site_id { get; set; }
+        public string site_name { get; set; }
+    }
+
+    public class Room
+    {
+        public string room_id { get; set; }
+        public string status { get; set; }
+    }
+
+    public class Agent
+    {
+        public int agent_id { get; set; }
+        public string agent_name { get; set; }
+        public string agent_display_name { get; set; }
+    }
+
+    public class Page
+    {
+        public string page_url { get; set; }
+        public string page_title { get; set; }
+    }
+
+    public class Visitor
+    {
+        public string visitor_id { get; set; }
+        public string user_name { get; set; }
+        public string external_key { get; set; }
+        public string email { get; set; }
+        public string company_name { get; set; }
+        public string tel { get; set; }
+        public string score { get; set; }
+        public string ip_addr { get; set; }
+        public string visit_count { get; set; }
+        public Page page { get; set; }
+        public string useragent { get; set; }
+    }
+}
