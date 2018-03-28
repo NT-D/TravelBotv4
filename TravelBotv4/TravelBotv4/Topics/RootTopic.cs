9c64579ed502e3d3424bfb41482aba0a8271b94d
MasatoSuzuki <masato-suzuki@navitime.co.jp>
2018-03-28 17:32:37 +0900

    Add chitchattopic to RootTopic

--- TravelBotv4/TravelBotv4/Topics/RootTopic.cs
+++ TravelBotv4/TravelBotv4/Topics/RootTopic.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Ai;
 using Microsoft.Bot.Builder.Core.Extensions;
 using Microsoft.Bot.Schema;
 using PromptlyBot;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 
@@ -17,82 +19,48 @@ namespace TravelBotv4.Topics
     {
         private const string SPOT_TOPIC = "searchSpotTopic";
         private const string PLAN_TOPIC = "searchPlanTopic";
+        private const string CHIT_CHAT_TOPIC = "chitChatTopic";
+        private const string ChitChatKey = "chitChatKey";
+        private QnAMaker qnAMaker;
 
         public RootTopic(IBotContext context) : base(context)
         {
-            /*
             // User state initialization should be done once in the welcome 
             //  new user feature. Placing it here until that feature is added.
-            if (context.GetUserState<BotUserState>().Alarms == null)
-            {
-                context.GetUserState<BotUserState>().Alarms = new List<Alarm>();
-            }
+            //if (context.GetUserState<BotUserState>().Alarms == null)
+            //{
+            //    context.GetUserState<BotUserState>().Alarms = new List<Alarm>();
+            //}
 
-            this.SubTopics.Add(ADD_ALARM_TOPIC, (object[] args) =>
+            this.SubTopics.Add(CHIT_CHAT_TOPIC, (object[] args) =>
             {
-                var addAlarmTopic = new AddAlarmTopic();
+                var chitChatTopic = new ChitChatTopic();
 
-                addAlarmTopic.Set
+                chitChatTopic.Set
                     .OnSuccess((ctx, alarm) =>
                     {
                         this.ClearActiveTopic();
 
-                        ctx.GetUserState<BotUserState>().Alarms.Add(alarm);
+                        //ctx.GetUserState<BotUserState>().Alarms.Add(alarm);
 
-                        context.SendActivity($"Added alarm named '{ alarm.Title }' set for '{ alarm.Time }'.");
+                        //context.SendActivity("test");
                     })
                     .OnFailure((ctx, reason) =>
                     {
                         this.ClearActiveTopic();
 
-                        context.SendActivity("Let's try something else.");
+                        context.SendActivity("FailureMessage");
 
-                        this.ShowDefaultMessage(ctx);
+                        //this.ShowDefaultMessage(ctx);
                     });
 
-                return addAlarmTopic;
+                return chitChatTopic;
             });
-
-            this.SubTopics.Add(DELETE_ALARM_TOPIC, (object[] args) =>
-            {
-                var alarms = (args.Length > 0) ? (List<Alarm>)args[0] : null;
-
-                var deleteAlarmTopic = new DeleteAlarmTopic(alarms);
-
-                deleteAlarmTopic.Set
-                    .OnSuccess((ctx, value) =>
-                    {
-                        this.ClearActiveTopic();
-
-                        if (!value.DeleteConfirmed)
-                        {
-                            context.SendActivity($"Ok, I won't delete alarm '{ value.Alarm.Title }'.");
-                            return;
-                        }
-
-                        ctx.GetUserState<BotUserState>().Alarms.RemoveAt(value.AlarmIndex);
-
-                        context.SendActivity($"Done. I've deleted alarm '{ value.Alarm.Title }'.");
-                    })
-                    .OnFailure((ctx, reason) =>
-                    {
-                        this.ClearActiveTopic();
-
-                        context.SendActivity("Let's try something else.");
-
-                        this.ShowDefaultMessage(context);
-                    });
-
-                return deleteAlarmTopic;
-            });
-            */
         }
 
-        
-        public override Task OnReceiveActivity(IBotContext context)
+
+        public async override Task OnReceiveActivity(IBotContext context)
         {
-            context.SendActivity("Got it!");
-         
             if ((context.Request.Type == ActivityTypes.Message) && (context.Request.AsMessageActivity().Text.Length > 0))
             {
                 var message = context.Request.AsMessageActivity();
@@ -100,16 +68,14 @@ namespace TravelBotv4.Topics
                 // If there is an active topic, let it handle this turn until it completes.
                 if (HasActiveTopic)
                 {
-                    ActiveTopic.OnReceiveActivity(context);
-                    return Task.CompletedTask;
+                    await ActiveTopic.OnReceiveActivity(context);
                 }
 
-                // 通常処理
+                await this.SetActiveTopic(CHIT_CHAT_TOPIC)
+                    .OnReceiveActivity(context);
             }
-            
-            return Task.CompletedTask;   
         }
-               
+
     }
 
 }
