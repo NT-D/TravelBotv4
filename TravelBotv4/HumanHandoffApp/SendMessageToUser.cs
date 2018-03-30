b258d07eb5307c8d2019869d64a2a0c82314a14d
Masayuki Ota <masota@microsoft.com>
2018-03-30 18:04:19 +0900

    Connect to chatplus

--- TravelBotv4/HumanHandoffApp/SendMessageToUser.cs
+++ TravelBotv4/HumanHandoffApp/SendMessageToUser.cs
@@ -14,6 +14,9 @@ using Microsoft.Bot.Connector;
 using Microsoft.WindowsAzure.Storage.Table;
 using Microsoft.WindowsAzure.Storage;
 using HumanHandoffApp.Models;
+using Microsoft.AspNetCore;
+using System.Net;
+using Microsoft.Extensions.Primitives;
 
 namespace HumanHandsoffApp
 {
@@ -25,7 +28,7 @@ namespace HumanHandsoffApp
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequest req,
             TraceWriter log)
         {
-            var account = CloudStorageAccount.Parse("<Your Connection String>");
+            var account = CloudStorageAccount.Parse("<Please insert your storage account key>");
             client = account.CreateCloudTableClient();
             CloudTable table = client.GetTableReference("ConversationInformation");
             await table.CreateIfNotExistsAsync();
@@ -38,16 +41,27 @@ namespace HumanHandsoffApp
             log.Info("C# HTTP trigger function processed a request.");
 
             //Parse body from ChatPlus' webhook
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            ChatPlusInformation webhookResponse = JsonConvert.DeserializeObject<ChatPlusInformation>(requestBody);
-            string name = webhookResponse.visitor.visitor_id;
+            //string requestBody = new StreamReader(req.Body).ReadToEnd();
+            //var data = body["data"];
+            //var res = JsonConvert.DeserializeObject<WebhookSendMessageResponse>(data);
+
+            var body = await req.ReadFormAsync();
+            body.TryGetValue("data", out StringValues bodydata);
+            string test = bodydata;
+            string decodedContent = WebUtility.UrlDecode(test);
+            var res = JsonConvert.DeserializeObject<ChatPlusInformation>(test);
+
+            //ChatPlusInformation webhookResponse = JsonConvert.DeserializeObject<ChatPlusInformation>(requestBody);
+            //string name = webhookResponse.visitor.visitor_id;
+            string name = "default user";
+
 
             //Get conversation reference from input binding
             //ConversationReference reference = JsonConvert.DeserializeObject<ConversationReference>(conversationInformationList.OrderByDescending(r => r.Timestamp).First().ConversationReference);
             //ConversationReference reference = JsonConvert.DeserializeObject<ConversationReference>(conversationInformation.ConversationReference);
 
             //Create Connector Client
-            var appCredential = new MicrosoftAppCredentials("<Your MicrosoftAppId which you set in application.json in TravelBot v4>", "<Your MicrosoftAppPassword which you set in application.json in TravelBot v4>");
+            var appCredential = new MicrosoftAppCredentials("<Microsoft App Id>", "Microsoft App Password");
             //string MicrosoftAppId = configuration["MicrosoftAppId"];
             //string MicrosoftAppPassword = configuration["MicrosoftAppPassword"];
             //var appCredential = new MicrosoftAppCredentials(MicrosoftAppId, MicrosoftAppPassword);
@@ -55,18 +69,21 @@ namespace HumanHandsoffApp
             var connector = new ConnectorClient(new Uri(reference.ServiceUrl), appCredential);
 
             //Compose activity
-            var proactiveMessage = Activity.CreateMessageActivity();
-            proactiveMessage.From = reference.Bot;
-            proactiveMessage.Conversation = reference.Conversation;
-            proactiveMessage.Text = webhookResponse.message.text;
+            if (res.type != "chat_start")
+            {
+                var proactiveMessage = Activity.CreateMessageActivity();
+                proactiveMessage.From = reference.Bot;
+                proactiveMessage.Conversation = reference.Conversation;
+                proactiveMessage.Text = res.message.text;
 
-            try
-            {
-                await connector.Conversations.SendToConversationAsync((Activity)proactiveMessage);
-            }
-            catch (Exception e)
-            {
-                Console.Write(e.Message);
+                try
+                {
+                    await connector.Conversations.SendToConversationAsync((Activity)proactiveMessage);
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e.Message);
+                }
             }
 
             //Return 200 or 400 to web client
