26ef721dd41d1684e14d6f2ec14f826dac3f930f
MasatoSuzuki <masato-suzuki@navitime.co.jp>
2018-03-30 16:53:34 +0900

    Add SelectQuestionLogic

--- TravelBotv4/TravelBotv4/Topics/RootTopic.cs
+++ TravelBotv4/TravelBotv4/Topics/RootTopic.cs
@@ -26,39 +26,15 @@ namespace TravelBotv4.Topics
         private static bool SearcherFeedbackState = false;
         private static bool SelectQuestionState = false;
         private static string qnaanswer = string.Empty;
-        private static string[] questionlist = null;
+        private static List<string> questionlist = new List<string>();
+        private static int turn = 0;
+        private static int maxturn = 1;
+        private static int i;
 
         public RootTopic(IBotContext context) : base(context)
         {
             // User state initialization should be done once in the welcome 
             //  new user feature. Placing it here until that feature is added.
-            this.SubTopics.Add("SelectQuestionPrompt", (object[] args) =>
-            {
-                var SelectQuesttionPrompt = new Prompt<string>();
-
-                SelectQuesttionPrompt.Set
-                    .OnPrompt(qnaanswer)
-                    .MaxTurns(2)
-                    .OnSuccess((ctx, value) =>
-                    {
-                        this.ClearActiveTopic();
-
-                        //this.State.Name = value;
-                        context.SendActivity(qnaanswer);
-
-                        this.OnReceiveActivity(context);
-                    })
-                    .OnFailure((ctx, reason) =>
-                    {
-                        this.ClearActiveTopic();
-
-                        context.SendActivity("I'm sorry I'm having issues understanding you.");
-
-                        this.OnReceiveActivity(context);
-                    });
-
-                return SelectQuesttionPrompt;
-            });
         }
 
 
@@ -87,14 +63,15 @@ namespace TravelBotv4.Topics
             if ((context.Request.Type == ActivityTypes.Message) && (context.Request.AsMessageActivity().Text.Length > 0))
             {
                 var message = context.Request.AsMessageActivity();
-
+                var qnamaker = new QnaMaker();
                 // If there is an active topic, let it handle this turn until it completes.
                 if (HasActiveTopic)
                 {
                     await ActiveTopic.OnReceiveActivity(context);
                     return;
                 }
-                if (!SearcherFeedbackState) {
+                if (!SearcherFeedbackState)
+                {
                     await context.SendActivity("Got it!");
                 }
 
@@ -106,17 +83,19 @@ namespace TravelBotv4.Topics
                     await context.SendActivity(answer);
                     return;
                 }
-                
+
                 // Feedback
                 if (SearcherFeedbackState)
                 {
                     SearcherFeedbackState = false;
                     var feedbacker = new Feedbacker();
                     var feedback = await feedbacker.SearchAsync(message.Text);
-                    if (feedback == Feedbacker.INTENT.FEEDBACK_NEGATIVE) {
+                    if (feedback == Feedbacker.INTENT.FEEDBACK_NEGATIVE)
+                    {
                         await context.SendActivity("Sorry, but could you try agein using another term?");
                         return;
-                    } else if (feedback == Feedbacker.INTENT.FEEDBACK_POSITIVE)
+                    }
+                    else if (feedback == Feedbacker.INTENT.FEEDBACK_POSITIVE)
                     {
                         await context.SendActivity("No problem!");
                         return;
@@ -124,43 +103,72 @@ namespace TravelBotv4.Topics
                     // Not reterun and continue next line when you get NOEN intent.
                 }
 
+                // SelectQuestion
+                if (SelectQuestionState)
+                {
+                    if (int.TryParse(message.Text, out i) && (i < 4))
+                    {
+                        var selectquestion = questionlist[i];
+                        var selectanswer = await qnamaker.SearchQnaMaker(selectquestion);
+                        await context.SendActivity(selectanswer.First().Answer);
+                        SelectQuestionState = false;
+                        SearcherFeedbackState = true;
+                        return;
+                    }
+                    else if (turn < maxturn)
+                    {
+                        await context.SendActivity("Sorry,but please input number(1 - 4)");
+                        turn += 1;
+                        return;
+                    }
+                    else
+                    {
+                        SelectQuestionState = false;
+                        await context.SendActivity("too many attempts");
+                        await context.SendActivity("OK! You may change your mind.");
+                        return;
+                    }
+                }
+
 
                 // QnA
-                var qnamaker = new QnaMaker();
+                qnamaker = new QnaMaker();
                 var queryresults = await qnamaker.SearchQnaMaker(message.Text);
-
                 if (queryresults != null)
+                {
                     if (queryresults.First().Questions.Count() == 1)
                     {
+                        SearcherFeedbackState = true;
                         await context.SendActivity(queryresults.First().Answer);
                         return;
                     }
                     else
                     {
+                        SelectQuestionState = true;
                         SearcherFeedbackState = true;
                         var messages = "Did you mean? Please input number(1 - 4)";
-
                         foreach (var q in queryresults.First().Questions.Select((value, index) => new { value, index }))
                         {
                             if (q.index > 2)
                             {
                                 messages += "\n\n" + "\n\n" + (q.index + 1) + ".None of adove";
+                                questionlist.Add(queryresults.First().Questions[q.index]);
                                 break;
                             }
                             messages += "\n\n" + "\n\n" + (q.index + 1) + "." + queryresults.First().Questions[q.index].ToString();
+                            questionlist.Add(queryresults.First().Questions[q.index]);
                         }
-                        qnaanswer = messages;
+                        await context.SendActivity(messages);
 
-                        await this.SetActiveTopic(SELECT_QUESTION_PROMPT)
-                                .OnReceiveActivity(context);
                         return;
                     }
-                
+                }
 
                 // Search
                 var finder = new Finder();
                 var result = await finder.SearchAsync(message.Text);
-                if (result != null) {
+                if (result != null)
+                {
                     SearcherFeedbackState = true;
                     var activity = createReply(context, result);
                     await context.SendActivity(activity);
